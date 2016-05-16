f61be76d51df6fecb8e18908ef5c4b8a11da6b23
yamadapc <tacla.yamada@gmail.com>
2016-05-16 16:50:24 -0300

    [temporary] Improvements

--- src/YesodSetup.hs
+++ src/YesodSetup.hs
@@ -1,9 +1,7 @@
 {-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards   #-}
-module YesodSetup
-    ( defaultMain
-    ) where
+module YesodSetup (defaultMain) where
 
 import           Control.Exception
 import qualified Data.ByteString.Char8        as ByteString
@@ -24,93 +22,114 @@ import           System.Directory.ProjectRoot
 import           System.Exit
 import           System.FilePath
 
-data DatabaseSettings = DatabaseSettings { dsHost     :: Text
-                                         , dsPort     :: Text
-                                         , dsUser     :: Text
-                                         , dsPassword :: Text
-                                         , dsName     :: Text
-                                         }
-    deriving(Generic, Show)
+data DatabaseSettings = DatabaseSettings
+    { dsHost     :: Text
+    , dsPort     :: Text
+    , dsUser     :: Text
+    , dsPassword :: Text
+    , dsName     :: Text
+    } deriving (Generic,Show)
 
 instance FromJSON DatabaseSettings where
-    parseJSON (Object o) = DatabaseSettings <$>
-        (dropStart <$> (o .: "host")) <*>
-        (dropStart <$> (o .: "port")) <*>
-        (dropStart <$> (o .: "user")) <*>
-        (dropStart <$> (o .: "password")) <*>
-        (dropStart <$> (o .: "database"))
+    parseJSON (Object o) =
+        DatabaseSettings <$> (dropStart <$> (o .: "host")) <*>
+                             (dropStart <$> (o .: "port")) <*>
+                             (dropStart <$> (o .: "user")) <*>
+                             (dropStart <$> (o .: "password")) <*>
+                             (dropStart <$> (o .: "database"))
       where
-        dropStart s =
-            let s' = Text.dropWhile (/= ':') s
-            in if Text.null s' then s
-               else let s'' = Text.dropWhile (/= ':') (Text.tail s')
-                    in if Text.null s'' then s'
-                       else Text.tail s''
+        dropStart s = let s' = Text.dropWhile (/= ':') s
+                      in if Text.null s'
+                         then s
+                         else let s'' = Text.dropWhile (/= ':') (Text.tail s')
+                              in if Text.null s''
+                                 then s'
+                                 else Text.tail s''
     parseJSON _ = fail "Couldn't parse"
+
+runSetup :: DatabaseSettings -> IO ()
+runSetup db = do
+    Text.putStrLn ("Connecting to " <> dsHost db <> "...")
+    conn <-
+        connectPostgreSQL
+            (Text.encodeUtf8
+                 ("host='" <> dsHost db <> "' " <> "port=" <> dsPort db <> "' " <>
+                  "user='postgres'" <>
+                  "password='postgres'"))
+    -- <> "dbname='postgres'"))
+    let handleSqlError (SqlError{..}) = do
+            setSGR [SetColor Foreground Vivid Yellow]
+            ByteString.putStrLn sqlErrorMsg
+            setSGR [Reset]
+            return 10
+    Text.putStrLn ("Creating role " <> dsUser db <> "...")
+    _ <-
+        execute_
+            conn
+            (fromString (Text.unpack ("create role \"" <> dsUser db <> "\""))) `catch`
+        handleSqlError
+    _ <-
+        execute_
+            conn
+            (fromString
+                 (Text.unpack ("alter role \"" <> dsUser db <> "\" with login"))) `catch`
+        handleSqlError
+    Text.putStrLn ("Creating database " <> dsName db <> "...")
+    _ <-
+        execute_
+            conn
+            (fromString
+                 (Text.unpack ("create database \"" <> dsName db <> "\""))) `catch`
+        handleSqlError
+    putStrLn "Setting password..."
+    _ <-
+        execute_
+            conn
+            (fromString
+                 (Text.unpack
+                      ("alter role \"" <> dsUser db <> "\" with password '" <>
+                       dsPassword db <>
+                       "'"))) `catch`
+        handleSqlError
+    putStrLn "Granting privileges..."
+    _ <-
+        execute_
+            conn
+            (fromString
+                 (Text.unpack
+                      ("grant all privileges on database \"" <> dsName db <>
+                       "\" to \"" <>
+                       dsUser db <>
+                       "\""))) `catch`
+        handleSqlError
+    return ()
 
 defaultMain :: IO ()
 defaultMain = do
     mpr <- getProjectRootCurrent
-    pr <- case mpr of
-        Just pr -> return pr
-        Nothing -> do
-            putStrLn "Couldn't find the project root"
-            getCurrentDirectory
-    putStrLn ("Looking for yesod settings in " ++ takeBaseName pr)
-    mvalue <- Yaml.decodeFile
-        (pr </> "config/settings.yml") :: IO (Maybe Value)
-    case mvalue of
-        Just (Object h) -> case HashMap.lookup "database" h of
-            Just database -> do
-                mdb <- Yaml.parseMonad parseJSON  database
-                    :: IO (Maybe DatabaseSettings)
-                case mdb of
-                    Nothing -> do
-                        putStrLn "Failed to parse settings.yml"
-                        exitFailure
-                    Just db -> do
-                        Text.putStrLn ("Connecting to " <> dsHost db <> "...")
-                        conn <- connectPostgreSQL (Text.encodeUtf8 (
-                                                "host='" <> dsHost db <> "' "
-                                                <> "port=" <> dsPort db <> "' "
-                                                <> "user='postgres'"
-                                                <> "dbname='postgres'"))
-
-                        let handleSqlError (SqlError{..}) = do
-                                setSGR [SetColor Foreground Vivid Yellow]
-                                ByteString.putStrLn sqlErrorMsg
-                                setSGR [Reset]
-                                return 10
-
-                        Text.putStrLn ("Creating role " <> dsUser db <> "...")
-                        _ <- execute_ conn
-                            (fromString (Text.unpack ("create role \"" <> dsUser db <> "\"")))
-                            `catch` handleSqlError
-                        _ <- execute_ conn
-                            (fromString (Text.unpack ("alter role \"" <> dsUser db <> "\" with login")))
-                            `catch` handleSqlError
-                        Text.putStrLn ("Creating database " <> dsName db <> "...")
-                        _ <- execute_ conn
-                            (fromString (Text.unpack ("create database \"" <> dsName db <> "\"")))
-                            `catch` handleSqlError
-                        putStrLn "Setting password..."
-                        _ <- execute_ conn
-                            (fromString (Text.unpack ("alter role \"" <>
-                                                      dsUser db <>
-                                                      "\" with password '" <>
-                                                      dsPassword db <> "'")))
-                            `catch` handleSqlError
-                        putStrLn "Granting privileges..."
-                        _ <- execute_ conn
-                            (fromString (Text.unpack ("grant all privileges on database \"" <>
-                                                      dsName db <>
-                                                      "\" to \"" <>
-                                                      dsUser db <> "\"")))
-                            `catch` handleSqlError
-                        return ()
+    pr <-
+        case mpr of
+            Just pr -> return pr
             Nothing -> do
-                putStrLn "Failed to parse settings.yml"
-                exitFailure
+                putStrLn "Couldn't find the project root"
+                getCurrentDirectory
+    putStrLn ("Looking for yesod settings in " ++ takeBaseName pr)
+    mvalue <-
+        Yaml.decodeFile (pr </> "config/settings.yml") :: IO (Maybe Value)
+    case mvalue of
+        Just (Object h) ->
+            case HashMap.lookup "database" h of
+                Just database -> do
+                    mdb <-
+                        Yaml.parseMonad parseJSON database :: IO (Maybe DatabaseSettings)
+                    case mdb of
+                        Nothing -> do
+                            putStrLn "Failed to parse settings.yml"
+                            exitFailure
+                        Just db -> runSetup db
+                Nothing -> do
+                    putStrLn "Failed to parse settings.yml"
+                    exitFailure
         _ -> do
             putStrLn "Failed to parse settings.yml"
             exitFailure
